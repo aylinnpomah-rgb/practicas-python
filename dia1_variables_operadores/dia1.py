4f834c1dc7db0ec3125afd18af92dcc0809c84d8
aylinnpomah-rgb <aylinnpomah@gmail.com>
2025-11-24 20:10:02 -0500

    Add basic variable and operator examples in Python

--- dia1_variables_operadores/dia1.py
+++ dia1_variables_operadores/dia1.py
@@ -1,1 +1,46 @@
+print('Hola Aylin, este es tu primer codigo')
+
+# VARIABLES
+print(type('hola')) # indica la clase de variable que es
+ #str: texto
+ #int: numeros enteros
+ #float: numeros decimales
+ #bool: indica si es verdadero o falso
+ #array-lista: indican una lista
+
+# OPERADORES
+
+print(3 + 6) #suma
+print(6 - 3) #resta
+print(6 * 3) #multiplicación
+print(6 / 3) #division
+print(12 % 4) #resto
+print(2 ** 10) #potencia
+print(11 // 3) #division sin residuo
+
+print('Hola ' + 'mundo')
+print('hola ' * 6)
+print('hola ' + str(6))
+print('hola ' + '6')
+
+a = 6
+print(str(a))
+
+# OPERADORES COMPARATIVOS
+
+print(4 > 8) # ¿4 es mayor que 8?
+print(4 < 8) # ¿4 es menor que 8?
+print(4 == 8) # ¿4 es igual que 8?
+print(4 >= 8) # ¿4 es mayor o igual que 8?
+print(4 <= 8) # ¿4 es menor o igual que 8?
+print(4 != 8) # ¿4 es diferente de 8?
+
+print('hola' > 'mundo') #empieza desde la primera letra y en el orden que va
+print('a' < 'p')
+print('aaa' < 'aab')
+
+# OPERADORES LOGICOS
+
+print( True and False) # and:con que exista un falso es falso
+print( True or False) #  or:con que exista un verdadero es verdadero
 
