04ce4d7eb469e816cc39c439ccd98e08b5bd2bcd
aylinnpomah-rgb <aylinnpomah@gmail.com>
2025-11-25 19:51:34 -0500

    ejercicios

---
+++ dia2_listas_tuplas_diccionarios/dia2.py
@@ -0,0 +1,19 @@
+#1-Dame el doble de un numero
+print('dame un numero')
+a = int(input()) 
+print( a * 2)
+
+a = int(input('dame un numero: '))
+print(a * 2)
+
+#2-Suma de 2 numeros
+x = int(input('dame un numero: '))
+y = int(input('dame otro numero: '))
+print(x + y)
+
+#es mayor de edad?
+a = int(input('dime tu edad: '))
+if a < 18 :
+    print('no eres mayor de edad')
+else :
+    print('eres mayor de edad')
